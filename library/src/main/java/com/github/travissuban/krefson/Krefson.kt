ac198bf4277754e4bbd7281568767f80b0669140
travis <travis.suban@gmail.com>
2016-11-06 03:11:08 +0700

    add default kerfson

--- library/src/main/java/com/github/travissuban/krefson/Krefson.kt
+++ library/src/main/java/com/github/travissuban/krefson/Krefson.kt
@@ -2,16 +2,11 @@ package com.github.travissuban.krefson
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.preference.PreferenceManager
 import com.google.gson.Gson
 
-open class Krefson @JvmOverloads constructor(context: Context, val name: String = "", val gson: Gson = Gson()) {
+class Krefson(context: Context, val name: String, val gson: Gson = Gson()) {
 
-    val sharedPreference: SharedPreferences = if (name == "") {
-        PreferenceManager.getDefaultSharedPreferences(context)
-    } else {
-        context.getSharedPreferences(name, Context.MODE_PRIVATE)
-    }
+    val sharedPreference: SharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE)
 
     inline operator fun <reified T> get(key: String): T? {
         return gson.fromJson(sharedPreference.getString(key, null))
@@ -31,5 +26,30 @@ open class Krefson @JvmOverloads constructor(context: Context, val name: String 
 
     fun remove(key: String) {
         sharedPreference.edit().remove(key).apply()
+    }
+
+    companion object {
+        lateinit var default: SharedPreferences
+        var gson: Gson = Gson()
+
+        inline operator fun <reified T> get(key: String): T? {
+            return gson.fromJson(default.getString(key, null))
+        }
+
+        inline operator fun <reified T> get(key: String, defaultValue: T): T {
+            return gson.fromJson(default.getString(key, null)) ?: defaultValue
+        }
+
+        inline operator fun <reified T> set(key: String, value: T) {
+            default.edit().putString(key, gson.toJson(value)).apply()
+        }
+
+        operator fun contains(key: String): Boolean {
+            return default.contains(key)
+        }
+
+        fun remove(key: String) {
+            default.edit().remove(key).apply()
+        }
     }
 }
