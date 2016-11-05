9fbfbdbdd6eec43af998b9e0e8e96e42d77da855
travis <travis.suban@gmail.com>
2016-11-06 03:11:08 +0700

    rename things

--- library/src/main/java/com/github/travissuban/krefson/Krefson.kt
+++ library/src/main/java/com/github/travissuban/krefson/Krefson.kt
@@ -6,26 +6,26 @@ import com.google.gson.Gson
 
 class Krefson(context: Context, val name: String, val gson: Gson = Gson()) {
 
-    val sharedPreference: SharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE)
+    val pref: SharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE)
 
     inline operator fun <reified T> get(key: String): T? {
-        return gson.fromJson(sharedPreference.getString(key, null))
+        return gson.fromJson(pref.getString(key, null))
     }
 
     inline operator fun <reified T> get(key: String, defaultValue: T): T {
-        return gson.fromJson(sharedPreference.getString(key, null)) ?: defaultValue
+        return gson.fromJson(pref.getString(key, null)) ?: defaultValue
     }
 
     inline operator fun <reified T> set(key: String, value: T) {
-        sharedPreference.edit().putString(key, gson.toJson(value)).apply()
+        pref.edit().putString(key, gson.toJson(value)).apply()
     }
 
     operator fun contains(key: String): Boolean {
-        return sharedPreference.contains(key)
+        return pref.contains(key)
     }
 
     fun remove(key: String) {
-        sharedPreference.edit().remove(key).apply()
+        pref.edit().remove(key).apply()
     }
 
     companion object {
